cf3aeb044863f9a226d97328efd768f341e36d93
venkateshprasad598 <venkateshprasad598@gmail.com>
2021-09-13 11:18:01 +0530

    Add

--- src/App.js
+++ src/App.js
@@ -1,36 +1,71 @@
-import "./App.css";
-import { useState } from "react";
-import data from "./data";
-import Menu from "./Menu";
-import Headline from "./Headline";
-import Filter from "./Filter";
+// import "./App.css";
+// import { useState } from "react";
+// import data from "./data";
+// import Menu from "./Menu";
+// import Headline from "./Headline";
+// import Filter from "./Filter";
 
-let cat = new Set(data.map((data) => data.category));
-let categ = ["All", ...cat];
-console.log(categ);
-function App() {
-  const [menu, setMenu] = useState(data);
+// let cat = new Set(data.map((data) => data.category));
+// let categ = ["All", ...cat];
+// console.log(categ);
+// function App() {
+//   const [menu, setMenu] = useState(data);
 
-  const filterData = (category) => {
-    if (category === "All") {
-      setMenu(data);
-    } else {
-      let filterdData = data.filter((data) => data.category == category);
-      setMenu(filterdData);
-    }
-  };
+//   const filterData = (category) => {
+//     if (category === "All") {
+//       setMenu(data);
+//     } else {
+//       let filterdData = data.filter((data) => data.category == category);
+//       setMenu(filterdData);
+//     }
+//   };
 
-  return (
-    <div className="App">
-      <Headline />
-      <Filter Filter={filterData} categ={categ} />
-      <div className="App__menu">
-        {menu.map((data) => {
-          return <Menu {...data} key={data.id} />;
-        })}
+//   return (
+//     <div className="App">
+//       <Headline />
+//       <Filter Filter={filterData} categ={categ} />
+//       <div className="App__menu">
+//         {menu.map((data) => {
+//           return <Menu {...data} key={data.id} />;
+//         })}
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+///TABS APP******************************************************************
+
+import React from "react";
+import { useState, useEffect } from "react";
+
+const App = () => {
+  const [exp, setExp] = useState([]);
+  const [value, setValue] = useState(0);
+  const [isLoading, setLoading] = useState(true);
+  const url = "https://course-api.com/react-tabs-project";
+  console.log(exp);
+  useEffect(() => {
+    // axios.get(url).then((response) => setExp(response.data));
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setExp(data));
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div>
+        <h1>Hello</h1>
       </div>
+    );
+  }
+
+  const { company, dates, duties, order, id, title } = exp[value];
+  return (
+    <div>
+      <h1>Hey</h1>
     </div>
   );
-}
-
+};
 export default App;
